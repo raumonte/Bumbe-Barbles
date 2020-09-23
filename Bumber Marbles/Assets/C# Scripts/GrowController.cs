9782c4b6b7cf0b2578b7af323694502f0ae92ddc
Darin Palermo <darpaler@uat.edu>
2020-09-23 15:57:10 -0700

    Added Growing and Base Pickups

---
+++ Bumber Marbles/Assets/C# Scripts/GrowController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowController : MonoBehaviour
+{
+    // Increases the player's scale.
+    public void Grow(float growthAmount)
+    {
+        transform.localScale += Vector3.one * growthAmount;
+    }
+}
---
+++ Bumber Marbles/Assets/C# Scripts/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is big enough to pick the object up.
+        if (collision.transform.localScale.x > transform.localScale.x)
+        {
+            OnPickup(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    // Called when someone picks up this item.
+    protected virtual void OnPickup(GameObject player)
+    {
+        // Do the thing.
+    }
+}
