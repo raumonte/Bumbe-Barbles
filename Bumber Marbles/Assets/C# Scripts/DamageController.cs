a74ea1ce362649d63c13fbb1ef4c47663ef30381
Darin Palermo <darpaler@uat.edu>
2020-09-23 17:22:12 -0700

    Started Setting Up Damage

---
+++ Bumber Marbles/Assets/C# Scripts/DamageController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    public GameObject debugObject;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Store the other colliders stats.
+        CharacterStats otherCharacterStats = collision.gameObject.GetComponent<CharacterStats>();
+
+        // If they don't have one, then don't do anything.
+        if (otherCharacterStats == null)
+        {
+            return;
+        }
+
+
+        Debug.Log(transform.localScale.x * gameObject.GetComponent<Rigidbody>().velocity.magnitude * Mathf.Clamp(Vector3.Dot(collision.gameObject.GetComponent<Rigidbody>().velocity, gameObject.GetComponent<Rigidbody>().velocity), 0, 1));
+
+        // Calculate damage.
+        otherCharacterStats.currentHealth -= transform.localScale.x * gameObject.GetComponent<Rigidbody>().velocity.magnitude * Mathf.Clamp(Vector3.Dot(collision.gameObject.GetComponent<Rigidbody>().velocity, gameObject.GetComponent<Rigidbody>().velocity), 0, 1);
+
+
+    }
+}
