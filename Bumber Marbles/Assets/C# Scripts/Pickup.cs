1968d39910b24f8fd325aa2c75cdba9af5cdbcf3
Raul Montes <montesraul7@gmail.com>
2020-09-27 07:27:23 -0700

    Made a few changes to scripts and scenes.

--- Bumber Marbles/Assets/C# Scripts/Pickup.cs
+++ Bumber Marbles/Assets/C# Scripts/Pickup.cs
@@ -6,33 +6,57 @@ public class Pickup : MonoBehaviour
 {
     public enum powerUp { healthRegen, doubleSpeed, meteor, intangibility}
     public powerUp selectedPowerUp;
+    public bool IsPoweredDash;
+    public float duration = 5f;
     private void Start()
     {
         selectedPowerUp = powerUp.healthRegen;
+        IsPoweredDash = false;
     }
-    //private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        //if (collision.gameObject.CompareTag("Player"))
+        //{
+        //    StartCoroutine( PickupAffect (collision) );
+        //}
+        //if (IsPoweredDash == false)
+        //{
+        //    IsPoweredDash = true;
+        //    OnActivate(collision.gameObject.GetComponent<CharacterStats>());
+        //}
+        //else if (IsPoweredDash == true)
+        //{
+        //    OnDeactivate(collision.gameObject.GetComponent<CharacterStats>());
+        //    IsPoweredDash = false;
+        //}
+
+        Destroy(this.gameObject);
+    }
+    //private void OnCollisionEnter(Collider other)
     //{
-    //    // Check if the player is big enough to pick the object up.
-    //    if (collision.transform.localScale.x > transform.localScale.x)
-    //    {
-    //        OnPickup(collision.gameObject);
-    //        Destroy(gameObject);
-    //    }
+    //    //if (other.gameObject.CompareTag("Player"))
+    //    //{
+    //    //    PickupAffect();
+    //    //}
+    //    OnActivate(other.gameObject.GetComponent<CharacterStats>());
+
     //}
-    private void OnTriggerEnter(Collider other)
-    {
-        PickupAffect();  
-    
-    }
-    void PickupAffect()
-    {
-        Debug.Log("Let Me Scream");
-        Destroy(gameObject);
-    }
+    //IEnumerator PickupAffect(Collision player)
+    //{
+    //    //CharacterStats stats = player.GetComponent<CharacterStats>();
+    //    //stats.ballTorque *= 2;
+    //    //stats.dashForce *= 2;
+    //    //yield return new WaitForSeconds(duration);
+    //    //stats.ballTorque /= 2;
+    //    //stats.dashForce /= 2;
+    //    //Debug.Log("Let Me Scream");
+    //    //Destroy(gameObject);
+    //}
     public void OnActivate(CharacterStats target)
     {
-        target.ballTorque *= 2;
-        target.dashForce *= 2;
+        target.ballTorque *= 2f;
+        target.dashForce *= 2f;
     }
     public void OnDeactivate(CharacterStats target)
     {
