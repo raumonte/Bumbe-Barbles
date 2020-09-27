4c62512f9d7be763296f869fc68d43ac1ea6d8fd
Raul Montes <montesraul7@gmail.com>
2020-09-27 03:44:15 -0700

    Started Pickup Script

--- Bumber Marbles/Assets/C# Scripts/Pickup.cs
+++ Bumber Marbles/Assets/C# Scripts/Pickup.cs
@@ -10,14 +10,34 @@ public class Pickup : MonoBehaviour
     {
         selectedPowerUp = powerUp.healthRegen;
     }
-    private void OnCollisionEnter(Collision collision)
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    // Check if the player is big enough to pick the object up.
+    //    if (collision.transform.localScale.x > transform.localScale.x)
+    //    {
+    //        OnPickup(collision.gameObject);
+    //        Destroy(gameObject);
+    //    }
+    //}
+    private void OnTriggerEnter(Collider other)
     {
-        // Check if the player is big enough to pick the object up.
-        if (collision.transform.localScale.x > transform.localScale.x)
-        {
-            OnPickup(collision.gameObject);
-            Destroy(gameObject);
-        }
+        PickupAffect();  
+    
+    }
+    void PickupAffect()
+    {
+        Debug.Log("Let Me Scream");
+        Destroy(gameObject);
+    }
+    public void OnActivate(CharacterStats target)
+    {
+        target.ballTorque *= 2;
+        target.dashForce *= 2;
+    }
+    public void OnDeactivate(CharacterStats target)
+    {
+        target.ballTorque /= 2;
+        target.dashForce /= 2;
     }
 
     // Called when someone picks up this item.
