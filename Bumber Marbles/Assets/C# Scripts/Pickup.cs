be5b7220c1d0edffd362123fe38ceffcb0886c76
Raul Montes <montesraul7@gmail.com>
2020-09-27 09:39:26 -0700

    Finalized Powerups and prefab for Powerups

--- Bumber Marbles/Assets/C# Scripts/Pickup.cs
+++ Bumber Marbles/Assets/C# Scripts/Pickup.cs
@@ -4,70 +4,30 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    public enum powerUp { healthRegen, doubleSpeed, meteor, intangibility}
-    public powerUp selectedPowerUp;
-    public bool IsPoweredDash;
+    //public enum powerUps { healthRegen, doubleSpeed, meteor, intangibility}
+    public PowerUp powerUp;
     public float duration = 5f;
     private void Start()
     {
-        selectedPowerUp = powerUp.healthRegen;
-        IsPoweredDash = false;
+      //  powerUp = powerUps.healthRegen;
+        GameManager.instance.currentPowerups.Add(this);
+    }
+    private void Update()
+    {
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
-
-        //if (collision.gameObject.CompareTag("Player"))
-        //{
-        //    StartCoroutine( PickupAffect (collision) );
-        //}
-        //if (IsPoweredDash == false)
-        //{
-        //    IsPoweredDash = true;
-        //    OnActivate(collision.gameObject.GetComponent<CharacterStats>());
-        //}
-        //else if (IsPoweredDash == true)
-        //{
-        //    OnDeactivate(collision.gameObject.GetComponent<CharacterStats>());
-        //    IsPoweredDash = false;
-        //}
-
-        Destroy(this.gameObject);
+        Vector3 origin = new Vector3(0, 0, 0);
+        PowerUpController powerUpController = collision.gameObject.GetComponent<PowerUpController>();
+        if (powerUpController != null)
+        {
+            powerUpController.AddPowerup(powerUp);
+            Destroy(this.gameObject);
+        }
     }
-    //private void OnCollisionEnter(Collider other)
-    //{
-    //    //if (other.gameObject.CompareTag("Player"))
-    //    //{
-    //    //    PickupAffect();
-    //    //}
-    //    OnActivate(other.gameObject.GetComponent<CharacterStats>());
-
-    //}
-    //IEnumerator PickupAffect(Collision player)
-    //{
-    //    //CharacterStats stats = player.GetComponent<CharacterStats>();
-    //    //stats.ballTorque *= 2;
-    //    //stats.dashForce *= 2;
-    //    //yield return new WaitForSeconds(duration);
-    //    //stats.ballTorque /= 2;
-    //    //stats.dashForce /= 2;
-    //    //Debug.Log("Let Me Scream");
-    //    //Destroy(gameObject);
-    //}
-    public void OnActivate(CharacterStats target)
+    void OnDestroy()
     {
-        target.ballTorque *= 2f;
-        target.dashForce *= 2f;
-    }
-    public void OnDeactivate(CharacterStats target)
-    {
-        target.ballTorque /= 2;
-        target.dashForce /= 2;
-    }
-
-    // Called when someone picks up this item.
-    protected virtual void OnPickup(GameObject player)
-    {
-        // Do the thing.
-
+        GameManager.instance.currentPowerups.Remove(this);
     }
 }
